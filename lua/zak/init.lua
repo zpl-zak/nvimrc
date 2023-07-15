4c420826b3c101b650d07a327e83c6c8cc0cd7b4
Dominik Madarasz <zaklaus@outlook.com>
2023-07-15 18:53:31 +0200

    Fix relative paths to configs

--- lua/zak/init.lua
+++ lua/zak/init.lua
@@ -1,14 +1,14 @@
 -- load lazy package manager
-require('configs.lazy')
+require('zak.configs.lazy')
 
 -- load configs
 local config_path = vim.fn.stdpath("config") .. "/lua/zak/configs"
 for _, file in ipairs(vim.fn.readdir(config_path, [[v:val =~ '\.lua$']])) do
   if file ~= "lazy.lua" then
-    require("configs."..file:gsub("%.lua$", ""))
+    require("zak.configs."..file:gsub("%.lua$", ""))
   end
 end
 
 -- load options
-require('options')
-require('keymap')
+require('zak.options')
+require('zak.keymap')
